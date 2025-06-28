2528eb962dcd043cc7f05cb77d77fe1eef32649a
RparraTZ <rparratellez@gmail.com>
2025-06-27 23:32:27 -0700

    Assigned id to city. Replace city for form input
    I assigned an Id to the h1 city element. Used JS in order to replace the existent html by the entered text in the form input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-
+function citySearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#display-city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", citySearchSubmit);
