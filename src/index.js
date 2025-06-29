c8d3ae7bf064f5cc06d31f84e27fdae67232c5a1
RparraTZ <rparratellez@gmail.com>
2025-06-28 23:51:18 -0700

    api key and url added and tested

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ function citySearchSubmit(event) {
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#display-city");
   cityElement.innerHTML = searchInput.value;
+  let city = searchInput.value;
+  return;
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", citySearchSubmit);
-let apiKey=18o7a0b8f4af4db5fa386d3ft8f43fea
+let apiKey = "18o7a0b8f4af4db5fa386d3ft8f43fea";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query={}&key=18o7a0b8f4af4db5fa386d3ft8f43fea";
