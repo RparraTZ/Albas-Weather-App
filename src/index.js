1d7bec04dedc13183cbbe80768b1e47f1e684830
RparraTZ <rparratellez@gmail.com>
2025-06-28 23:31:17 -0700

    Axios added and tested

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ function citySearchSubmit(event) {
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", citySearchSubmit);
+let apiKey=18o7a0b8f4af4db5fa386d3ft8f43fea
