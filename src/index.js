8908c10edc66bdd17e1534e85788d9eb022615a1
RparraTZ <rparratellez@gmail.com>
2025-07-02 01:42:31 -0700

    Added Js to refresh city name and current temp
    Edited a little of html and made js code to display city name and current temperature in the inner html of the correct elements.
    tested and  working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+function displayWeatherData(response) {
+  let currentTemp = Math.round(response.data.temperature.current);
+  currentTempDisplay.innerHTML = currentTemp;
+  let cityNameDisplay = response.data.city;
+  cityElement.innerHTML = cityNameDisplay;
+  //console.log(response.data.city);
+}
 function citySearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#display-city");
-  cityElement.innerHTML = searchInput.value;
-  let city = searchInput.value;
-  return;
+  let cityEntered = searchInput.value;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityEntered}&key=18o7a0b8f4af4db5fa386d3ft8f43fea&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherData);
 }
 let searchForm = document.querySelector("#search-form");
+let searchInput = document.querySelector("#search-input");
+let currentTempDisplay = document.querySelector("#current-tepm-display");
+let cityElement = document.querySelector("#display-city");
 searchForm.addEventListener("submit", citySearchSubmit);
-let apiKey = "18o7a0b8f4af4db5fa386d3ft8f43fea";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={}&key=18o7a0b8f4af4db5fa386d3ft8f43fea";
+
+//let apiKey = "18o7a0b8f4af4db5fa386d3ft8f43fea";
