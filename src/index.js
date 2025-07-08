e4b37be429b0ffae55224208132fbcc66ed6a4fd
RparraTZ <rparratellez@gmail.com>
2025-07-07 20:24:29 -0700

    added humidity, conditions and windspeed
    sucesfully coded js to display humidity index, conditions and windspeed of input city in search form. tested and working

--- src/index.js
+++ src/index.js
@@ -3,18 +3,37 @@ function displayWeatherData(response) {
   currentTempDisplay.innerHTML = currentTemp;
   let cityNameDisplay = response.data.city;
   cityElement.innerHTML = cityNameDisplay;
-  //console.log(response.data.city);
+  let currentConditionDesc = response.data.condition.description;
+  let currentHumidity = response.data.temperature.humidity;
+  let currentWindSpeed = response.data.wind.speed;
+  descriptionElement.innerHTML = currentConditionDesc;
+  humidityRate.innerHTML = `${currentHumidity} %`;
+  windSpeed.innerHTML = `${currentWindSpeed} mph`;
+  formatDate(response.data.time);
 }
-function citySearchSubmit(event) {
-  event.preventDefault();
-  let cityEntered = searchInput.value;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityEntered}&key=18o7a0b8f4af4db5fa386d3ft8f43fea&units=imperial`;
+function searchCity(city) {
+  let apiKey = "18o7a0b8f4af4db5fa386d3ft8f43fea";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherData);
 }
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  searchCity(searchInput.value);
+}
+function formatDate(date) {
+  now = new date();
+  day = now.getDay();
+  console.log(day);
+  //let now=
+
+  //let dayNames=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+}
 let searchForm = document.querySelector("#search-form");
-let searchInput = document.querySelector("#search-input");
 let currentTempDisplay = document.querySelector("#current-tepm-display");
 let cityElement = document.querySelector("#display-city");
-searchForm.addEventListener("submit", citySearchSubmit);
-
-//let apiKey = "18o7a0b8f4af4db5fa386d3ft8f43fea";
+let descriptionElement = document.querySelector("#description");
+let humidityRate = document.querySelector("#humidity-unit");
+let windSpeed = document.querySelector("#wind-speed");
+//console.log(windSpeed);
+searchForm.addEventListener("submit", handleSearchSubmit);
