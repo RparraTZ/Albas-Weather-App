34814230d810feceb5d89574b58a8ff992f9458f
RparraTZ <rparratellez@gmail.com>
2025-07-15 00:49:58 -0700

    added code to display forecast
    I added the code to display api data for  the 5 day forecast logo, and temperature using function displayForescast. and created function formatDay to display correct day name .

--- src/index.js
+++ src/index.js
@@ -53,22 +53,30 @@ function getForecast(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 function displayForecast(response) {
-  console.log(response.data);
-
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="forecast-day-containers">
-            <div class="forecast-days">${day}</div>
-            <div class="forecast-icon">🌦️</div>
-            <div class="forecast-hilow">
-              <div class="forecast-hi-Temp">19°</div>
-              <div class="forecast-low-temp">10°</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+            <div class= "weather-forecast-temperatures">
+            <div class= "weather-forecast-temperature-max">
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
             </div>
-          </div>`;
+            <div class="weather-forecast-temperature-min">
+           ${Math.round(day.temperature.minimum)}°</div>
+            </div>
+            </div>
+            `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
